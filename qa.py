00ef3161474975ea50e94ea0fd419bd691ffb184
Broever101 <mrassam258@gmail.com>
2021-05-26 20:22:34 +0500

    Debugging

--- qa.py
+++ qa.py
@@ -1,3 +1,4 @@
+from ir import Document, Query
 from typing import Any
 from transformers import AutoModelForQuestionAnswering
 from transformers import AutoTokenizer
@@ -9,11 +10,12 @@ class QuestionAnswering:
         self.model = AutoModelForQuestionAnswering.from_pretrained(model_ckpt)
         self.tokenizer = AutoTokenizer.from_pretrained(custom_tokenizer)
         self.pipeline = QuestionAnsweringPipeline(model=self.model, tokenizer=self.tokenizer, \
-                                        framework='pt', device=1)
+                                        framework='pt', device=0)
 
-    def answer(self, passage: str, query: str) -> dict:
-        result_set = {"book" : passage.book, "chapter" : passage.chapter_no}
-        result = self.pipeline(context=str(passage), query=query.text)
+    def answer(self, passage: Document, query: Query) -> dict:
+        result_set = {"book" : passage.book, "chapter" : passage.chapter_no, \
+                        "context" : passage.text}
+        result = self.pipeline(context= str(passage), question= query.text)
         result_set['answer'] = result['answer']
         result_set['score'] = result['score']
         return result_set
