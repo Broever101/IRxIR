8408fa098d86f7035e87707da0efe1b217425a18
Broever101 <mrassam258@gmail.com>
2021-05-26 20:22:23 +0500

    Debugging

--- model.py
+++ model.py
@@ -1,18 +1,24 @@
+from pprint import pprint
+from typing import List, Dict
 from qa import QuestionAnswering
 from ir import Query, DocumentRetrieval, PassageRetrieval
 
-model_ckpt = 'ktrapeznikov/albert-xlarge-v2-squad-v2'
+model_ckpt = 'distilbert-base-uncased-distilled-squad'
 
 qa_model = QuestionAnswering(model_ckpt, model_ckpt)
 dr = DocumentRetrieval()
 
-def predict(question: str) -> list[dict]:
+def predict(question: str) -> List[Dict]:
     query = Query(question)
     documents = dr.retrieve(query)
     pr = PassageRetrieval(documents)
     passages = pr.retrieve(query)
     results = []
     for passage in passages:
-        result = qa_model.answer(str(passage), query.text)
+        result = qa_model.answer(passage, query)
         results.append(result)
     return results 
+
+if __name__ == "__main__":
+    query = "Who is Judah"
+    pprint(predict(query))
